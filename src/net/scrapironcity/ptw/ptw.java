74f36efd2565d1323d23aca9ad01fc0432f57309
JadedDragoon <JadedDragoon@users.noreply.github.com>
2020-05-10 03:24:32 -0500

    implemented PlayerWorldcChangeEvent listenrr

--- src/net/scrapironcity/ptw/ptw.java
+++ src/net/scrapironcity/ptw/ptw.java
@@ -1,15 +1,20 @@
 package net.scrapironcity.ptw;
 
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ptw extends JavaPlugin {
+	private Listener worldChangeListener = new WorldChangeListener();
+	
     @Override
     public void onEnable() {
     	this.getCommand("ptw").setExecutor(new CommandMain());
+    	getServer().getPluginManager().registerEvents(worldChangeListener, this);
     }
     
     @Override
     public void onDisable() {
-
+    	HandlerList.unregisterAll(worldChangeListener);
     }
 }
